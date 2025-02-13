9b5802f7c784338b30a2071ea8f9fa58d02ef6f6
jaychouwj <74359147+jaychouwj@users.noreply.github.com>
2025-02-13 21:44:11 +0800

    Add files via upload

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("王杰 && 曹煜荧")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("送给最可爱最漂亮最猛女的老婆大人")
+    .pause(3000)
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
